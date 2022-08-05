f1ba0f57ea58c6a2723e51b71b7f8fa55eb13e92
Cristian <naitsirc1101@gmail.com>
2022-08-05 17:26:19 -0500

    init app journal

---
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="d-flex">
+      <div class="col-4">
+        <EntryList />
+      </div>
+      <div class="col">
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue';
+
+export default {
+  name: 'DayBookLayout',
+  components: {
+    Navbar: defineAsyncComponent(() => import('../components/NavBar')),
+    EntryList: defineAsyncComponent(() => import('../components/EntryList')),
+  },
+};
+</script>
+
+<style scoped></style>
